6c00933a43a01e40c52afe3162c6378f57311e2c
thelittlehawk <faik.catibusic@live.com>
2015-02-10 13:13:33 +0100

    New message change from Mac OS X

--- server.js
+++ server.js
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Pozdrav raja. Sta ima?\n');
+    res.end('Pozdrav raja.\nSta ima? Nema nista.\n');
 }).listen(port);
